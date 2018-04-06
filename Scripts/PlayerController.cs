11848fba5a49ed34c21d9617ba7728b6c16fc1ec
REEEEEEEEEEEEEEYE <37923026+REEEEEEEEEEEEEEYE@users.noreply.github.com>
2018-04-06 17:07:34 -0600

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float PlayerSpeed;
+
+	void Start(){
+		
+	}
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal") * PlayerSpeed * Time.deltaTime;
+		float moveVertical = Input.GetAxis ("Vertical") * PlayerSpeed * Time.deltaTime;
+
+		transform.Translate (new Vector3 (moveHorizontal, moveVertical));
+	}﻿
+}
